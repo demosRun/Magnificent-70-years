43d24454089268cd58f5cdca750ac208651d9cc3
puge <mail@puge.org>
2019-06-19 15:38:10 +0800

    确认版

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,3 +1,15 @@
 
 $(function () {
+  var swiper = new Swiper('.swiper-container-pPX8XxuUyIehAAEu', {
+    autoplay: 3000,
+    loop: true,
+    pagination: '.pagination-pPX8XxuUyIehAAEu',
+    paginationClickable: true
+  })
+  $('.last.button').click(function(e) {
+    swiper.swipePrev();
+  })
+  $('.next.button').click(function(e) {
+    swiper.swipeNext();
+  })
 })
